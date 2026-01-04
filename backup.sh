c74755f1c7c280ec18cd93b70902f9f769402157
Dio-Lopes <diogo20083@gmail.com>
2026-01-04 14:31:24 +0000

    Minor script change

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
-# This script backs up the world using 7zip
+# This script backs up the world using 7zip ultra compression
 cd /home/dio/Minecraft\ Server/
 NOW=$( date '+%F_%H:%M:%S' )
 7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on $NOW.7z world
